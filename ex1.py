097d5b02fedb2a4866f2f5dc36592c5dbdf83c00
Hackbright Student <info@hackbrightacademy.com>
2014-02-11 12:39:09 -0800

    First and final, solved really quickly

---
+++ ex1.py
@@ -0,0 +1,19 @@
+import string
+import os
+import shutil
+
+alphabet = string.lowercase
+alphabet = list(alphabet)
+
+#os.mkdir("test")
+
+#for letter in alphabet:
+#    os.mkdir(letter)
+
+#shutil.move("original_files/aexwin.txt", "a")
+
+for fn in os.listdir("original_files"):
+    first_letter = fn[0]
+    #print os.getcwd()
+    sourcefile =  "original_files/"+fn
+    shutil.move(sourcefile, first_letter)
